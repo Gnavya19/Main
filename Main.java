e646a35731d64f66336301734615f1c466c90ae0
Gnavya19 <navyaguddeti19@gmail.com>
2025-01-31 09:45:18 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Sum sc =new Sum();
+        Sum c =new Sum();
+        
+        System.out.println(sc.a+sc.b);
+        System.out.println(c.a+c.b);
+    }
+    
+}
